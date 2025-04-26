645bed223e4d3736367fe3ba614e9efdfbed815b
manamasu <97730788+manamasu@users.noreply.github.com>
2025-04-26 15:27:50 +0200

    refactor: writing records works now with a generic record type-struct + csv has now the correct scheme

--- main.go
+++ main.go
@@ -10,15 +10,18 @@ import (
 
 const filename = "problems.csv"
 
+type Record[T any] struct {
+	record []T
+}
+
 func main() {
 	fmt.Println("Hello Quizmaster")
 	generateCSVMathProblemsFile(filename)
-	generateMathProblemRecord()
 }
 
 func generateMathProblemRecord() [][]int {
 	src := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(src)
+	r := rand.New(src) //Salting (Randomization) based on our src
 
 	numbers := make([][]int, 0, 200)
 
@@ -43,10 +46,18 @@ func generateCSVMathProblemsFile(filename string) {
 	}
 	defer file.Close()
 
-	headers := []string{"Summand1", "Summand2", "Sum"}
+	headers := Record[string]{record: []string{"Summand1", "Summand2", "Sum"}}
 	writeCSVRecord(writer, headers)
+
+	numbers := generateMathProblemRecord()
+
+	for _, v := range numbers {
+		mathRecord := Record[int]{record: v}
+		writeCSVRecord(writer, mathRecord)
+	}
 }
 
+// Creates a file f in the current folder and passes the csv writer w through
 func createCSVWriter(filename string) (*csv.Writer, *os.File, error) {
 
 	f, err := os.Create(filename)
@@ -60,9 +71,18 @@ func createCSVWriter(filename string) (*csv.Writer, *os.File, error) {
 	return w, f, nil
 }
 
-func writeCSVRecord(writer *csv.Writer, record []string) {
-	err := writer.Write(record)
+// writeCSVRecord writes a Record[T] to the given CSV writer.
+// converts each field to a string before writing, and flushes (writes to csv).
+// T can be any type that can be stringified using fmt.Sprint.
+func writeCSVRecord[T any](writer *csv.Writer, rec Record[T]) {
+	var stringRecord []string
+	for _, v := range rec.record {
+		stringRecord = append(stringRecord, fmt.Sprint(v))
+	}
+
+	err := writer.Write(stringRecord)
 	if err != nil {
 		fmt.Println("Error writing record to csv: ", err)
 	}
+	writer.Flush()
 }
